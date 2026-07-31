86872fd6babfb5a4efbf67a0f303e5780f578ac7
Mlima411 <mateuslc411@gmail.com>
2026-07-31 14:43:37 -0400

    test: adiciona teste de adicao de produto ao carrinho

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,20 +1,28 @@
-describe('Teste de Navegação no E-commerce', () => {
+describe('Fluxo de Compras - Ecommerce', () => {
+  beforeEach(() => {
+    // Visita o site de testes de e-commerce (SauceDemo)
+    cy.visit('https://www.saucedemo.com/')
+  })
 
-  it('Deve abrir o site do e-commerce e validar o carregamento', () => {
-    // Abre um site de e-commerce real de testes
-    cy.visit('https://saucedemo.com');
-    
-    // O robô preenche o usuário sozinho
-    cy.get('[data-test="username"]').type('standard_user');
-    
-    // O robô preenche a senha sozinha
-    cy.get('[data-test="password"]').type('secret_sauce');
-    
-    // O robô clica no botão de entrar
-    cy.get('[data-test="login-button"]').click();
-    
-    // Valida se entramos na página de produtos com sucesso
-    cy.get('.title').should('contain', 'Products');
-  });
+  it('Deve realizar o login e adicionar um produto ao carrinho com sucesso', () => {
+    // 1. Fazer Login
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
 
-});
+    // 2. Valida se entrou na página de produtos
+    cy.url().should('include', '/inventory.html')
+    cy.get('.title').should('have.text', 'Products')
+
+    // 3. Adicionar o primeiro produto ao carrinho
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+    // 4. Validar que o contador do carrinho subiu para 1
+    cy.get('.shopping_cart_badge').should('have.text', '1')
+
+    // 5. Entrar no carrinho e verificar se o produto está lá
+    cy.get('.shopping_cart_link').click()
+    cy.url().should('include', '/cart.html')
+    cy.get('.inventory_item_name').should('contain', 'Sauce Labs Backpack')
+  })
+})
